d8b1cf8060da538452835a64bf1a8405c3da1cde
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-13 14:30:29 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // Chiedo all'utente il numero di chilometri che vuole percorrere
 let km = Number(prompt('Quanti chilometri vuoi percorrere?'));
+document.getElementById('km').innerHTML = `Distanza da percorrere: ${km} km`;
 
 // Chiedo all'utente la sua età
 let age = Number(prompt('Quanti anni hai?'));
+document.getElementById('age').innerHTML = `Età: ${age} anni`;
 
 // Prezzo del biglietto al km
 let price = 0.21;
@@ -25,15 +27,17 @@ if (isNaN(km) || isNaN(age)) {
 
         // Calcolo il prezzo per gli over 65
         let discountedTicket = ticketPrice - priceOver65;
-        console.log('Prezzo over 65: ' + discountedTicket.toFixed(2));
+        document.getElementById('ticket').innerHTML = `Prezzo biglietto (sconto over 65): ${discountedTicket.toFixed(2)} &euro;`;
     } else if (age < 18) {
 
         // Calcolo il prezzo per i minorenni
         let discountedTicket = ticketPrice - priceUnder18;
+        document.getElementById('ticket').innerHTML = `Prezzo biglietto (sconto minorenni): ${discountedTicket.toFixed(2)} &euro;`;
         console.log('Prezzo minorenni: ' + discountedTicket.toFixed(2));
     } else {
 
         // Prezzo biglietto standard
+        document.getElementById('ticket').innerHTML = `Prezzo biglietto standard: ${ticketPrice} &euro;`;
         console.log('Prezzo standard: ' + ticketPrice);
     }
 }
